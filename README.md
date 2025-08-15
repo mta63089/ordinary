dfa7c822143bebfd99e11092e8a1f1ff9729f3cf
mta630 <mta630@ddubb.net>
2025-08-15 15:31:24 -0500

    trace animation test

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ordinary
+
+## Credits
+
+"Retro Television - Mitsubishi" (<https://skfb.ly/osxZx>) by Alfking49 is licensed under Creative
+Commons Attribution (<http://creativecommons.org/licenses/by/4.0/>).
--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,51 +1,14 @@
 "use client"
 
-import { motion } from "framer-motion"
-
+import PathDrawing from "@/components/draw-logo"
 import BallPit from "@/components/play-area"
-import { Television } from "@/components/television"
 
 export default function HomePage() {
   return (
     <main className="bg-background flex min-h-svh flex-col justify-between overflow-hidden p-8">
-      <div className="h-svh w-svh">
-        <Television />
-      </div>
+      <div className="h-svh w-svh"></div>
       <div className="flex flex-1 flex-col gap-y-20">
-        <motion.h2
-          initial={{ color: "#111", x: 500 }}
-          animate={{
-            color: ["#109d", "#93ef11", "#214a32"],
-            x: [0, 500, 0],
-          }}
-          transition={{ duration: 3, repeat: Infinity, ease: "easeInOut" }}
-          className="h-svh overflow-hidden font-bold"
-        >
-          ORDINARY
-        </motion.h2>
-        <motion.h2
-          initial={{ y: -1000, x: -1000, opacity: 0 }}
-          whileInView={{
-            backgroundColor: "#214a32",
-            opacity: 1,
-            x: 0,
-            y: 0,
-          }}
-          transition={{ duration: 2, type: "spring" }}
-          className="h-svh overflow-hidden font-bold"
-        >
-          CREATIVE
-        </motion.h2>
-        <motion.h2
-          initial={{ y: 100 }}
-          animate={{
-            translateX: 100,
-          }}
-          transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
-          className="overflow-hidden font-bold"
-        >
-          STUDIO
-        </motion.h2>
+        <PathDrawing />
       </div>
       <BallPit />
     </main>
